70be8f9047b30243711879fdbea1f19fbfdf278d
Darth Vader: Sith Lord <dmitrii95@outlook.com>
2021-11-11 12:34:56 +0300

    commit0

---
+++ WebFileManager/Controllers/FilesControllers/FilesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebFileManager.Models;
+using WebFileManager.Models.FilesModels;
+using System.IO;
+
+
+namespace WebFileManager.Controllers
+{
+    public class FilesController : Controller
+    {
+        public static Disk disk = new Disk();
+        [HttpGet]
+        public IActionResult Files()
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo("C:\\");
+            if (directoryInfo.Exists)
+            {
+                var a=directoryInfo.GetDirectories();
+            }
+            return View("~/Views/FilesViews/Files.cshtml");
+        }
+
+    }
+}
---
+++ WebFileManager/Models/FilesModels/Disk.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WebFileManager.Models.FilesModels
+{
+    public class Disk
+    {
+        public decimal TotalFreeSpace { get; }
+        public decimal TotalSize { get; }
+        public List<DriveInfo> Disks { get; }
+
+
+        public Disk()
+        {
+            Disks = DriveInfo.GetDrives().Where(r => r.IsReady).Where(r => r.DriveType == DriveType.Fixed).ToList();
+            TotalSize = Disks.Sum(r => r.TotalSize);
+            TotalFreeSpace = Disks.Sum(r => r.TotalFreeSpace);
+        }
+    }
+    public static class DiskMetods
+    {
+        /// <summary>
+        /// возвращает число в килобайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в килобайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToKB(this decimal size, int decimals = 2)
+        {
+            return Math.Round(size / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в мегабайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в мегабайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToMB(this decimal size, int decimals = 2)
+        {
+            return Math.Round(size.ToKB(0) / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в гигобайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в гигобайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToGB(this decimal size, int decimals = 2)
+        {
+            return Math.Round(size.ToMB(0) / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в терабайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в теробайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToTB(this decimal size, int decimals = 2)
+        {
+            return Math.Round(size.ToGB(0) / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в килобайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в килобайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToKB(this long size, int decimals = 2)
+        {
+            return Math.Round((decimal)size / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в мегабайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в мегабайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToMB(this long size, int decimals = 2)
+        {
+            return Math.Round(size.ToKB(0) / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в гигобайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в гигобайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToGB(this long size, int decimals = 2)
+        {
+            return Math.Round(size.ToMB(0) / 1024, decimals);
+        }
+        /// <summary>
+        /// возвращает число в терабайтах
+        /// </summary>
+        /// <param name="size">число для преобразования в теробайты</param>
+        /// <param name="decimals">количество знаков после запятой</param>
+        /// <returns></returns>
+        public static decimal ToTB(this long size, int decimals = 2)
+        {
+            return Math.Round(size.ToGB(0) / 1024, decimals);
+        }
+    }
+}
